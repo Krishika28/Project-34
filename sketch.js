e47b24264d20fa233e55a8148b0800030bab292c
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2020-09-27 14:34:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,18 +4,22 @@ var dog, happyDog, database, food, foodStock, dog_img, happy_img;
 function preload()
 {
   //load images here
-  dog_img = loadImage("images/dogImg");
-  happy_img= loadImage("images/dogImg1");
-
-  foodStock = database.ref('Food');
-  foodStock.on("value",readStock);
+  dog_img = loadImage("images/dogImg.png");
+  happy_img= loadImage("images/dogImg1.png");
 }
 
 function setup() {
+  database = firebase.database();
+  
   createCanvas(500, 500);
   
+   foodStock = database.ref('Food');
+  foodStock.on("value",readStock);
+  
   dog = createSprite(250,250,20,20);
-
+dog.addImage(dog_img);
+   dog.scale= 0.3;
+  
 }
 
 
@@ -35,6 +39,8 @@ if(keyDown === UP_ARROW){
   textSize(20);
   fill(118, 252, 250);
   text("Note: Press UP_ARROW key to feed the dog",400,370);
+  
+   text("Food remaining : "+foodS,170,200);
 
 }
 function readStock(data){
